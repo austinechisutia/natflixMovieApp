eb632a0009ee6396a038d5a4c0bb83c16e417b3a
austinechisutia <austineakhonya624@gmail.com>
2025-02-18 10:05:44 +0300

    a fresh start

--- js/api.js
+++
@@ -1,4 +0,0 @@
-let api_key = "78fde742e72bb82915a97d3e08494df7";
-let img_url = "https://image.tmdb.org/t/p/w500";
-let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
-let movie_genre_http = "https://api.themoviedb.org/3/discover/movie?"
