f41376dbbc8575efa9698eb569062d0c0476ff11
austinechisutia <austineakhonya624@gmail.com>
2025-02-18 12:11:55 +0300

    movies on the screen

--- public/js/api.js
+++ public/js/api.js
@@ -2,3 +2,4 @@ let api_key = "78fde742e72bb82915a97d3e08494df7";
 let image_url ="https://image.tmdb.org/t/p/w500";
 
 let genre_list_http = 'https://api.themoviedb.org/3/genre/movie/list?';
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
